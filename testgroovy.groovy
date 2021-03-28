9e1b45aa41480ca616c0314e896776a3e477561b
Aarthi002 <72254890+Aarthi002@users.noreply.github.com>
2021-03-28 13:05:46 +0530

    Update testgroovy.groovy

--- testgroovy.groovy
+++ testgroovy.groovy
@@ -1,12 +1,11 @@
 pipeline {
   agent any
   environment {
-    //once you sign up for Docker hub, use that user_id here
-    registry = "katara123/devops"
-    //- update your credentials ID after creating credentials for connecting to Docker Hub
-    registryCredential = 'katara123'
-    dockerImage = ''
-  }
+		PROJECT_ID = 'tasko-task'
+    CLUSTER_NAME = 'k8s-cluster'
+    LOCATION = 'us-central1-c'
+    CREDENTIALS_ID = 'tasko-task'		
+	}
   stages {
     stage ('Code Pull') {
       steps{
@@ -16,17 +15,6 @@ pipeline {
       }
     }
     
-    /*stage('PyLint: Code Analysis') {
-      steps {
-        script {
-          sh 'pylint sample.py | tee pylint.log'
-          recordIssues(
-            tool: pyLint(pattern: 'pylint.log'),
-            failTotalHigh: 10,
-          )
-        }
-      }
-    }*/
     
     stage ('test: Unit-Test') {
       steps{
@@ -68,36 +56,39 @@ pipeline {
       }
     }
     
-    stage('Building image') {
-      steps{
-        script {
-          dockerImage = docker.build registry
-        }
-      }
-    }
-    stage('Upload Image - DockerHub') {
-      steps{
-        script {
-          docker.withRegistry( '', registryCredential ) {
-            dockerImage.push()  
-          }
-        }
-      }
-    }
-    // Stopping Docker containers for cleaner Docker run
-    stage('Clean Container') {
-      steps {
-        sh 'docker ps -f name=mypythonappContainer -q | xargs --no-run-if-empty docker container stop'
-        sh 'docker container ls -a -fname=mypythonappContainer -q | xargs -r docker container rm'
-      }
-    }
-    // Running Docker container, make sure port 80 mapped from flask server port 5000 is opened in
-    stage('Docker Run') {
-      steps{
-        script {
-          dockerImage.run("-p 0.0.0.0:80:5000/tcp --rm --name mypythonappContainer")
-        }
-      }
-    }
+    stage('Build Docker Image') {
+		    steps {
+			    sh 'whoami'
+			    script {
+				    myimage = docker.build("katara123/devops:${env.BUILD_ID}")
+			    }
+		    }
+	    }
+    stage("Push Docker Image") {
+		    steps {
+			    script {
+				    echo "Push Docker Image"
+				    withCredentials([string(credentialsId: 'katara123', variable: 'katara123')]) {
+            				sh "docker login -u katara123 -p ${katara123}"
+				    }
+				        myimage.push("${env.BUILD_ID}")
+				    
+			    }
+		    }
+	    }
+     stage('Deploy to K8s') {
+		    steps{
+			    echo "Deployment started ..."
+			    sh 'ls -ltr'
+			    sh 'pwd'
+			    sh "sed -i 's/tagversion/${env.BUILD_ID}/g' serviceLB.yaml"
+				sh "sed -i 's/tagversion/${env.BUILD_ID}/g' deployment.yaml"
+			    echo "Start deployment of serviceLB.yaml"
+			    step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'serviceLB.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+				echo "Start deployment of deployment.yaml"
+				step([$class: 'KubernetesEngineBuilder', projectId: env.PROJECT_ID, clusterName: env.CLUSTER_NAME, location: env.LOCATION, manifestPattern: 'deployment.yaml', credentialsId: env.CREDENTIALS_ID, verifyDeployments: true])
+			    echo "Deployment Finished ..."
+		    }
+	    }
   }
 }
